a9e77924f11ab6675118dd418af54bf05fcb3dd3
akaneotake <akane1033@gmail.com>
2025-03-02 22:50:20 +0700

    Add user-id to session

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_02_153818) do
+ActiveRecord::Schema.define(version: 2025_03_02_154711) do
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
