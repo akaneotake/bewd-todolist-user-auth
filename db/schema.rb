4104bd78666e8a53a417aa93cca95b71632c014e
akaneotake <akane1033@gmail.com>
2025-03-02 22:46:23 +0700

    Add user controller & create sessions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_02_21_091538) do
+ActiveRecord::Schema.define(version: 2025_03_02_153818) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
