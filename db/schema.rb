5547556f2a9ac7c7f145f4f3a3bd00de4c5ceb4f
akaneotake <akane1033@gmail.com>
2025-03-02 22:58:39 +0700

    Add token to session & generate session token & connect user and session

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_02_154711) do
+ActiveRecord::Schema.define(version: 2025_03_02_155111) do
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "token"
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
