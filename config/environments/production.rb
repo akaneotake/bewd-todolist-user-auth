84ecf304abead2f587e0b6ac36a112073d4565de
akaneotake <akane1033@gmail.com>
2025-04-24 16:45:52 +0200

    Allow render host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Render用
+  config.hosts << "ruby-todolist.onrender.com"
+  
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
